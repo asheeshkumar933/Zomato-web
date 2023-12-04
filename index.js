f09cf5872bb5307fab9931689e71f675adcda453
Asheesh Sharma <144527342+asheeshkumar933@users.noreply.github.com>
2023-12-04 11:53:05 +0000

    fix bugs

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ window.addEventListener('click',function(event){
 
 
 })
+<<<<<<< HEAD
   
 //  login validation cheack
 
@@ -51,6 +52,123 @@ function cheack() {
     }
     return false;
   }
+=======
+
+document.getElementById('login').addEventListener('click' ,login )
+document.getElementById('signin').addEventListener('click' ,Signup )
+
+// validation cheack
+
+function cheack() {
+
+  let x = document.getElementById("numb").value;
+  // If x is Not a Number or blank input field
+  let text;
+  if (isNaN(x) ||x==="") {
+    
+    text = " invalid Input";
+   
+   
+  } else {
+  
+  document.getElementById('demo').innerHTML =  getRndInteger(1000,9999)
+
+
+  
+  }
+  document.getElementById("demo1").innerHTML = text;
+}
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// submit button
+
+
+function validateForm() {
+  let x = document.forms ["uform"]["oname"].value;
+  if (x==="") {
+    alert(" OTP is  required");
+   
+  }
+  else{
+    confirm(" Login is succesfull Click the ok button");
+    // window.location.pathname =
+    //  "./website/afterlogin.html";
+    window.location.href ="./afterlogin.html"
+    
+   
+  }
+  return false;
+}
+
+
+
+
+
+
+// sign up code here
+function Signup() {
+  document.getElementById("id02").style.display = "block";
+}
+
+// Get the modal1
+
+let modal1 = document.getElementById("id02");
+
+
+window.addEventListener('click',function(event){
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+
+
+})
+ 
+
+// sign up code ends here
+
+
+// pizza page jump  code
+//  let pizza=document.querySelectorAll('#pizza')
+// pizza.forEach(function(e){
+  
+//   e.addEventListener('click' ,function(){
+   
+
+//     window.location.href='./add to cart/pizza_addtocart.html';
+      
+ 
+//   })
+// })
+//  let biryani=document.querySelectorAll('#biryani')
+// biryani.forEach(function(e){
+  
+//   e.addEventListener('click' ,function(){
+    
+//     window.location.href =
+//     "./add to cart/Biryani.html";
+   
+   
+//   })
+// })
+//  let burger=document.querySelectorAll('#Burger')
+// burger.forEach(function(e){
+  
+//   e.addEventListener('click' ,function(){
+//     window.location.pathname =
+//     "./add to cart/Burger.html";
+   
+    
+  
+//   })
+// })
+
+
+
+
+
+>>>>>>> b37c098 (fix bugs)
 
 // form SIGN UP 
   function signvalidateForm() {
@@ -169,6 +287,8 @@ order_buttons.addEventListener('click',function(event){
 
 });
 })
+
+
 
 
 
